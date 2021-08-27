bd4b5e4f3cc1c547573b1ebf23db1d0c5380a6b9
Faisal-AlDhuwayhi <alduohy11@gmail.com>
2021-08-27 22:37:22 +0300

    Process the data

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,81 @@
+# import libraries
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Load and merge the messages and categories data
+
+    Args:
+        messages_filepath ([string]): file path of the messages data file
+        categories_filepath ([string]): file path of the categories data file
+
+    Returns:
+        dataframe: a merged dataframe of the messages and categories data
+    """
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = categories.merge(messages, on=['id'])
+
+    return df
 
 
 def clean_data(df):
-    pass
+    """Clean and reorganize the data 
+
+    Args:
+        df ([dataframe]): a dataframe containing the data
+
+    Returns:
+        dataframe: the dataframe after cleaning
+    """
+    # split categories column into separate category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    # extract the columns names from the first row
+    row = categories.iloc[0, :]
+    category_colnames = list(row.apply(lambda x: x[:-2]))
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # convert category values to just numbers 0 or 1.
+    for column in categories:
+        # set each value to be the last character of the string (which is the number)
+        categories[column] = categories[column].astype(str).str[-1]
+        
+        categories[column] = pd.to_numeric(categories[column])
+
+    # the related category has small amount of value 2. It is converted to the majority class which is one,
+    # to have only 1 and 0 values.
+    categories['related'] = categories['related'].apply(lambda x: 1 if x == 2 else x)
+
+    # replace categories column in df with new category columns.
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+
+    # remove duplicates
+    df = df[~df.duplicated()]
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """Save the dataframe to a database
+
+    Args:
+        df ([dataframe]): a dataframe containing the data
+        database_filename ([string]): a database file path
+    """
+    
+    # create connection with the database
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    # upload the dataframe to the database
+    df.to_sql('disaster_response_table', engine, index=False)
 
 
 def main():
